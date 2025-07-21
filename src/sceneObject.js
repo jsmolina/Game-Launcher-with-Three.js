1e2379fdcfad402c7c8879d2975f428497ce3ca6
onepopcorn <marc.feliu@gmail.com>
2025-07-21 11:36:13 +0200

    chore: adds common code style

---
+++ src/sceneObject.js
@@ -0,0 +1,16 @@
+// Class scene object
+export class SceneObject {
+  name = '';
+  id;
+  focus = false;
+  mesh;
+  constructor(name) {
+    this.name = name;
+    this.id = 0;
+    this.focus = false;
+  }
+
+  isFocused() {
+    return this.focus;
+  }
+}
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
+import path from 'path';
+
 /**
  * @type {import('vite').UserConfig}
  */
-export default{
-    assetsInclude:['**/*glb'],
+export default {
+    root: './src',
+    assetsInclude: ['**/*glb'],
     base: process.env.NODE_ENV === 'production' ? '' : '',
     build: {
         chunkSizeWarningLimit: 60000,
         assetsInlineLimit: 0
-    }
+    },
 
 }
